6f9ecf1c5d3c576bed38fa6a28c334c2fe7dc5d5
Obed Aguilar Lujan <97857376+Churros99@users.noreply.github.com>
2023-10-23 09:37:42 -0600

    add: problemas con compatibiidad con el metodo de seguridad

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # imports
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, login_required
+from werkzeug.security import check_password_hash
 
 #  aaplication init
 app = Flask(__name__)
@@ -32,6 +33,10 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     password = db.Column(db.String(80))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # create tables
 with app.app_context():
     db.create_all()
@@ -42,12 +47,24 @@ def home():
     products = Product.query.all()
     return render_template('home.html', products = products)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        
+        return 'Invalid username or password'
+    
     return render_template('login.html')
 
 # Products Routes
 @app.route('/products', methods=['POST', 'GET'])
+@login_required
 def create_product():
     try:
         if request.method == 'POST':
@@ -65,6 +82,7 @@ def create_product():
         print(inst.args)
 
 @app.route('/products/<int:product_id>', methods=['PUT'])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -76,6 +94,7 @@ def update_product(product_id):
     return redirect(url_for('home'))
 
 @app.route('/products/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
 def delete_product(id):
     product = Product.query.get(id)
     if product:
